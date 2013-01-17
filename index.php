7b160cdd45afd88b14b92808b7efce423d5eb2d2
Brian Jensen <mail@magnify.dk>
2013-01-17 21:21:18 +0100

    Added files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Neverwinter DK</title>
+  <link rel="icon" type="image/png" href="favicon.ico">
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/styles.css" />
+</head>
+<body>
+  <a href="http://www.facebook.dk/NeverwinterDK"><img src="neverwinter-dk.png" width="900" height="400" alt=""></a>
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-37759468-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script> 
+</body>
+</html>
